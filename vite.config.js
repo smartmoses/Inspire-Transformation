c33dec8e8d5bc10754f6408de7d42729dfb0b8ce
Smart Moses <censusokoi515@gmail.com>
2025-09-21 18:47:51 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'serve' ? '/' : '/Inspire-Transformation/',
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      manifest: true,
-      rollupOptions: {
-        output: {
-          manualChunks: undefined
-        }
-      }
-    },
-    server: {
-      port: 3000,
-      open: true,
-      host: true
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
+  // FIX: Set the base path for deployment
+  base: '/', 
 })
